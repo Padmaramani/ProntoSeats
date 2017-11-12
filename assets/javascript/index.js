746f7e094791063352f5170a89feb3ac1362f4ef
hornd24 <david.horn689@gmail.com>
2017-11-11 20:58:25 -0800

    adding funtions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,30 +1,115 @@
-var  guestNameWait;
-var guestNameTable;
+var  guestName
+var numAdults;
+var numChild;
+var guestinfo;
+var serversnum;
+var tables;
+var serverName;
+var serverPin;
+var timeOff;
+server=[];
+
+ var dt = new Date();
+
+    var time = dt.getHours() + ":" + dt.getMinutes();
+   dt.format('shorttime')
+
+   //tIn = time in.
+   //tOut = time out
+function Server(name,tIn,pin,tOut){
+this.name =name;
+this.timeIn = tIn;
+this.serverPin=pin;
+this.timeOut =tOut;
+
+this.addToList =function(){
+    
+  
+
+};
+
+};
+
+function Guestinfo(name,aduts,child){
+    this.guestName=name;
+    this.numAdults=aduts;
+    this.numChild=child;
+
+    this.setguestinfo = function(){
+
+        guestinfo= 'Guest Name: ' + this.guestName +'<br>'+ 'Adults: ' + this.numAdults+ '<br>'+ 'Children: ' + this.numChild;
+        
+      
+    };
+
+    this.assignTable = function(){
+        $('#guestinfo').html(guestinfo)
+    }
+};
+
 function pushWaitlist(){
-     guestNameWait = $('#guestName').val();
-    //  $('#test').html(guestName);
+    guestName = $('#guestName').val();
+    numAdults = $('#adults').val();
+    numChild =$('#children').val();
+    var newguest = new Guestinfo(guestName,numAdults,numChild);
+    
      $('#guestName').val('');
 
 window.location.assign('waitlist.html');
 
 };
 function openTables(){
-    guestNameTable = $('#guestName').val();
+    guestName = $('#guestName').val();
     numAdults = $('#adults').val();
     numChild =$('#children').val();
-    $('#guestinfo').html('Guest Name: ' + guestNameTable +'<br>'+ 'Adults: ' + numAdults+ '<br>'+ 'Children: ' + numChild);
+    var newguest = new Guestinfo(guestName,numAdults,numChild);
+    newguest.setguestinfo();
+    newguest.assignTable();
+    
     $('#children').val('');
     $('#guestName').val('');
     $('#adults').val('');
-   
+     
+    createGrid(4,10);
 }
 $('#wait').on('click',function(){
     
     pushWaitlist();
-  
+    newguest.setguestinfo();
 
 
 });
 $('#tables').on('click',function(){
    openTables();
 });
+$('#serverlist').on('click',function(){
+    
+})
+// function that builds a grid in the "container"
+function createGrid(serversnum,tables) {
+    for (var rows = 0; rows < serversnum; rows++) {
+        for (var columns = 0; columns < tables; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(200);
+    $(".grid").height(200);
+    
+};
+function signInServer (){
+    serverName =  $('#servername').val();
+serverPin = $('#serverpin').val();
+timeOff=$('#timeoff').val();
+ var  newserver = new Server(serverName,time,serverPin,timeOff);
+ var addName = $('<td>');
+ addName.addClass('serverNamesIn');
+  addName.append(newserver.name);
+  $('#list1').append(addName);
+ 
+}
+$('#signin').on('click',function(){
+    $('#exampleModal').modal('hide')
+    signInServer();
+    
+})
+$('#bottom').append(time)
